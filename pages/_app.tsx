04fa48bb74f596f913d445fe2af1ff13aa23e7ab
abhishek22331 <abhishek.immanent@gmail.com>
2023-10-17 18:31:01 +0530

    build error solving

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -13,18 +13,22 @@ export default function App({ Component, pageProps }: AppProps) {
           integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
           crossOrigin="anonymous"
         />
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
         <script
+          async
+          src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"
+        ></script>
+        <script
+          async
           src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
           integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
           crossOrigin="anonymous"
         ></script>
         <script
+          async
           src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
           integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+"
           crossOrigin="anonymous"
         ></script>
-        
       </Head>
       <Component {...pageProps} />
     </>
